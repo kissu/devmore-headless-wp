495299b76ae34d87aa45b55d6810d7261462374b
Diana <diana.sofie.nilsson@gmail.com>
2021-12-11 20:58:28 +0100

    Add page content and recent blog posts

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
             ? 'Skräddarsydda IT-lösningar'
             : index === 1
             ? 'Vi skräddarsyr det optimala systemet för just ert företag'
-            : 'Hur kan ni optimera ert företag?'
+            : 'Hur vill ni optimera ert företag?'
         "
         :key="image.id"
         :img-src="image.media_details.sizes.full.source_url"
@@ -25,6 +25,40 @@
         v-for="(image, index) in heroImages"
       />
     </b-carousel>
+
+    <main>
+      <!-- Page content -->
+      <section class="page-content text-center">
+        <div v-html="page.content.rendered" />
+        <b-button variant="info">Mer om oss</b-button>
+      </section>
+
+      <!-- Latest blogposts -->
+      <section class="text-center">
+        <h2>Senaste blogginläggen</h2>
+        <div class="posts-container">
+          <b-card
+            class="post-card mx-3"
+            :title="post.title.rendered"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            :key="post.id"
+            v-for="post in recentPosts"
+          >
+            <div v-html="post.excerpt.rendered" />
+            <b-button :to="`/blogg/${post.slug}`" variant="info">
+              Läs mer
+            </b-button>
+          </b-card>
+        </div>
+
+        <b-button class="mt-5" to="/blogg" variant="dark">
+          Visa alla blogginlägg
+        </b-button>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -41,5 +75,41 @@ export default {
     );
     return { heroImages };
   },
+  computed: {
+    page() {
+      return this.$store.state.pages.find((el) => el.slug === "start");
+    },
+    recentPosts() {
+      return this.$store.state.posts.slice(0, 3);
+    },
+  },
 };
 </script>
+<style scoped>
+.page-content {
+  margin: auto;
+  max-width: 60rem;
+}
+
+.posts-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.post-card {
+  max-width: 20rem;
+}
+
+@media (max-width: 1200px) {
+  .posts-container {
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .post-card {
+    max-width: 40rem;
+    margin-bottom: 2rem;
+  }
+}
+</style>
