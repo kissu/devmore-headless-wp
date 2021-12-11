c954c7ea89d558ceca3ec60b4a7deca591939ffb
Diana <diana.sofie.nilsson@gmail.com>
2021-12-11 20:56:50 +0100

    Add pages to store

--- store/index.js
+++ store/index.js
@@ -1,8 +1,12 @@
 export const state = () => ({
+  pages: [],
   posts: [],
 });
 
 export const mutations = {
+  updatePages: (state, pages) => {
+    state.pages = pages;
+  },
   updatePosts: (state, posts) => {
     state.posts = posts;
   },
@@ -13,9 +17,12 @@ export const actions = {
   async nuxtServerInit({ commit, state }) {
     if (state.posts.length) return;
     try {
-      const posts = (
-        await this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/posts")
-      )
+      const [pages, posts] = await Promise.all([
+        this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/pages"),
+        this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/posts"),
+      ]);
+
+      posts
         .filter((el) => el.status === "publish")
         .map(({ id, slug, title, excerpt, date, tags, content }) => ({
           id,
@@ -26,6 +33,16 @@ export const actions = {
           tags,
           content,
         }));
+
+      pages.map(({ id, slug, title, date, tags, content }) => ({
+        id,
+        slug,
+        title,
+        date,
+        tags,
+        content,
+      }));
+      commit("updatePages", pages);
       commit("updatePosts", posts);
     } catch (err) {
       console.log(err);
