03b7c647f863ea3c05254fe970c66f2e56058ddc
Diana <diana.sofie.nilsson@gmail.com>
2021-12-13 08:06:46 +0100

    Add tags and media to store

--- store/index.js
+++ store/index.js
@@ -1,14 +1,56 @@
 export const state = () => ({
+  media: [],
   pages: [],
   posts: [],
+  tags: [],
 });
 
 export const mutations = {
+  updateMedia: (state, media) => {
+    state.media = media;
+  },
   updatePages: (state, pages) => {
-    state.pages = pages;
+    state.pages = pages
+      .filter((el) => el.status === "publish")
+      .map(({ id, slug, title, date, tags, content }) => ({
+        id,
+        slug,
+        title,
+        date,
+        tags,
+        content,
+      }));
   },
   updatePosts: (state, posts) => {
-    state.posts = posts;
+    state.posts = posts
+      .filter((el) => el.status === "publish")
+      .map(
+        ({
+          id,
+          featured_media,
+          slug,
+          title,
+          excerpt,
+          date,
+          tags,
+          content,
+        }) => ({
+          id,
+          featured_media,
+          slug,
+          title,
+          excerpt,
+          date,
+          tags,
+          content,
+        })
+      );
+  },
+  updateTags: (state, tags) => {
+    state.tags = tags.map(({ id, name }) => ({
+      id,
+      name,
+    }));
   },
 };
 
@@ -17,33 +59,17 @@ export const actions = {
   async nuxtServerInit({ commit, state }) {
     if (state.posts.length) return;
     try {
-      const [pages, posts] = await Promise.all([
+      const [media, pages, posts, tags] = await Promise.all([
+        this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/media"),
         this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/pages"),
         this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/posts"),
+        this.$axios.$get("http://localhost/devmore/wp-json/wp/v2/tags"),
       ]);
 
-      posts
-        .filter((el) => el.status === "publish")
-        .map(({ id, slug, title, excerpt, date, tags, content }) => ({
-          id,
-          slug,
-          title,
-          excerpt,
-          date,
-          tags,
-          content,
-        }));
-
-      pages.map(({ id, slug, title, date, tags, content }) => ({
-        id,
-        slug,
-        title,
-        date,
-        tags,
-        content,
-      }));
+      commit("updateMedia", media);
       commit("updatePages", pages);
       commit("updatePosts", posts);
+      commit("updateTags", tags);
     } catch (err) {
       console.log(err);
     }
