564606c4a5a4f265adf1774c133bb5156dc12034
Diana <diana.sofie.nilsson@gmail.com>
2021-12-14 22:24:44 +0100

    Remove pages

--- pages/case.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>Case</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style>
--- pages/jobba-pa-devmore.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>Jobba på Devmore</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style>
